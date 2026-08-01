47c5c4d7d2dc0b5641dbe7813bed1753f9c9e422
Neiman Marcus <engineermarcus72@gmail.com>
2026-08-01 13:05:22 +0300

    add autoupdate script

---
+++ autoupdate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+REPO="engineermarcus/magpie"
+FILE="downloader.py"
+LOCAL="$(dirname "$0")/$FILE"
+BRANCH="main"
+
+remote_hash=$(curl -sf "https://raw.githubusercontent.com/$REPO/$BRANCH/$FILE" | md5sum | cut -d' ' -f1)
+local_hash=$(md5sum "$LOCAL" | cut -d' ' -f1)
+
+if [ "$remote_hash" != "$local_hash" ]; then
+    echo "[update] Change detected — pulling latest..."
+    git -C "$(dirname "$0")" pull origin "$BRANCH"
+else
+    echo "[update] Up to date."
+fi
